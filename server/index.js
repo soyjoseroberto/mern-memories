bfad0bacf2ed9b06d604d83ef212a81bb0b9eb47
soyjoseroberto <jrodriguez18@gmail.com>
2022-03-10 00:18:27 -0500

    Server express app and mongo setup done.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-import express from "express"
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+// Mongo Cloud Atlas
+const CONNECTION_URL = "mongodb+srv://soyjoseroberto:desarrollo507@cluster0.djvoo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+// Mongoose
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+   .catch((error) => console.log(`${error} did not connect`));
+ 
+// mongoose.set('useFindAndModify', false);
